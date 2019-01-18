f0bb8d91df65ef2116f25efca4d1750f6a567d4f
Pupastis <bralic.tina@gmail.com>
2019-01-18 14:39:17 +0100

    Add files via upload

---
+++ php prvi zadatak/index.php
@@ -0,0 +1,55 @@
+<?php 
+print '
+<!DOCTYPE html>
+<html>
+	<head>
+		
+		<!-- CSS -->
+		<link rel="stylesheet" href="style.css">
+		<!-- End CSS -->
+		<!-- meta elements -->
+		<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <meta name="description" content="php">
+        <meta name="keywords" content="keyword 1, keyword 2, keyword 3, keyword 4, ...">
+		
+		<title>PHP web programiranje</title>
+	</head>
+<body>
+	<header>
+		<div'; if ($_GET['menu'] > 1) { print ' class="hero-subimage"'; } else { print ' class="hero-image"'; }  print '></div>
+		<nav>
+			<ul>
+				<li><a href="index.php?menu=1">Home</a></li>
+				<li><a href="index.php?menu=2">News</a></li>
+				<li><a href="index.php?menu=3">Contact</a></li>
+				<li><a href="index.php?menu=4">About</a></li>
+				<li><a href="index.php?menu=5">Gallery</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main>';
+		
+	# Homepage
+	if (!isset($_GET['menu']) || $_GET['menu'] == 1) { include("home.php"); }
+	
+	# News
+	else if ($_GET['menu'] == 2) { include("news.php"); }
+	
+	# Contact
+	else if ($_GET['menu'] == 3) { include("contact.php"); }
+	
+	# About us
+	else if ($_GET['menu'] == 4) { include("about-us.php"); }
+	
+	# Gallery
+	else if ($_GET['menu'] == 5) { include("gallery.php"); }
+	
+		print '
+	</main>
+	<footer>
+		<p>Copyright &copy; ' . date("Y") . ' Tina Bralic. <a href="https://github.com/Pupastis/"><img src="img/GitHub-Mark-Light-32px.png" title="Github" alt="Github"></a></p>
+	</footer>
+</body>
+</html>';
+?>
---
+++ php prvi zadatak/news.php
@@ -0,0 +1,24 @@
+<?php 
+	print '
+	<h1>NEWS</h1>
+		<div class="news">
+			<a href="news-3.html"><img src="news/news-3.jpeg" alt="Plitvice zimi" title="Plitvice zimi"></a>
+			<h2>Izlet na plitvicka jezera</h2>
+			<p>Nacionalni park Plitvička jezera je najveći, najstariji i najposjećeniji hrvatski nacionalni park, a predstavlja šumovit planinski kraj u kojem se nalazi 16 jezera <a href="news-3.html">More ...</a></p>
+			<p><time datetime="2019-01-14">14 January 2019</time></p>
+			<hr>
+			<a href="news-2.html"><img src="news/news-2.jpg" alt="Best hiking gear" title="Best hiking gear"></a>
+			<h2>Best hiking gear</h2>
+			<p>Don’t waste your money on gear that’s no good;  <a href="news-2.html">More ...</a></p>
+			<p><time datetime="2019-01-14">14 January 2019</time></p>
+			<hr>
+			<a href="news-1.html"><img src="news/news-1.jpg" alt="" title="Velebit hiking trail"></a>
+			<h2>Velebit hiking trail</h2>
+			<p>9-day trekking through the most beautiful croatian mountain <a href="news-1.html">More ...</a></p>
+			<p><time datetime="2019-01-14">14 January 2019</time></p>
+			
+			</div>
+			';
+	
+
+?>
